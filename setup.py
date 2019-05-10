fb709e729ddded4ccb5aca41e5c2774b557ac586
Lars Martens <lars.martens97@gmail.com>
2019-05-10 20:53:08 +0200

    Fit for PyPi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="sprich",
+    version="0.1",
+    author="Lars Martens",
+    author_email="contact@haselkern.com",
+    description="A compiler for interactive dialogs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haselkern/sprich",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+)
